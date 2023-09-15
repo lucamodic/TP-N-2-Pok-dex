f0953b5b0032c20c446b339433d4a06d786d4780
Usuario <pablot.pavan@gmail.com>
2023-09-15 09:18:59 -0300

    arreglado conexion

--- pokedex.sql
+++ pokedex.sql
@@ -1,3 +1,9 @@
 CREATE SCHEMA IF NOT EXISTS pokedex;
 USE pokedex;
-CREATE TABLE pokemon(id INT AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE);
+CREATE TABLE pokemon(
+    id INT AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
+    num_id INT,
+    imagen VARCHAR(50),
+    nombre VARCHAR(50),
+    tipo VARCHAR(50),
+    descripcion VARCHAR(50));
