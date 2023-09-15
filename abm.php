5aa5507dfb274ad8058259de86ef975f047f5adc
Usuario <pablot.pavan@gmail.com>
2023-09-15 14:38:26 -0300

    inserts

--- abm.php
+++ abm.php
@@ -1,9 +1,7 @@
 <?php
 $conexion = mysqli_connect("localhost", "root", "", "pokedex")
 or exit("No se pudo conectar a la base de datos");
-$sql = "INSERT INTO pokemon (num_id, imagen, nombre, tipo, descripcion)
-VALUES (1,'../imagenes/ico_3ds_001','Bulbasaur','planta','...')";
+$sql = "SELECT * FROM pokemon";
 $resultado=$conexion->query($sql);
-
 
 mysqli_close($conexion);
--- pokedex.sql
+++ pokedex.sql
@@ -7,3 +7,8 @@ CREATE TABLE pokemon(
     nombre VARCHAR(50),
     tipo VARCHAR(50),
     descripcion VARCHAR(50));
+INSERT INTO `pokemon` (`id`, `num_id`, `imagen`, `nombre`, `tipo`, `descripcion`) VALUES
+(1, 1, '../imagenes/ico_3ds_001', 'Bulbasaur', 'planta', '...'),
+(2, 2, '../imagenes/ico_3ds_002', 'Ivysaur', 'planta', '...'),
+(3, 3, '../imagenes/ico_3ds_003', 'Venusaur', 'planta', '...'),
+(4, 4, '../imagenes/ico_3ds_004', 'Charmander', 'fuego', '...');
