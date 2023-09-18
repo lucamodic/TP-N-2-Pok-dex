7071bff5491045b89d0261781248fb6e3b68f426
Usuario <pablot.pavan@gmail.com>
2023-09-18 08:47:22 -0300

    pequeño arreglo

--- abm.php
+++ abm.php
@@ -1,11 +1,7 @@
 <?php
-$config = parse_ini_file('config.ini', true);
-$password = $config['clave'];
-$conexion = mysqli_connect("localhost", "root", "$password", "pokedex")
+$conexion = mysqli_connect("localhost", "root", "", "pokedex")
 or exit("No se pudo conectar a la base de datos");
-    $sql = "INSERT INTO pokemon (num_id, imagen, nombre, tipo, descripcion)
-    VALUES (1,'../imagenes/ico_3ds_001','Bulbasaur','planta','...')";
-    $resultado=$conexion->query($sql);
-
+$sql = "SELECT * FROM pokemon";
+$resultado=$conexion->query($sql);
 
 mysqli_close($conexion);
