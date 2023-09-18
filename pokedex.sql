e34ee38c8f1dc4f413b8eeb21fd1a107673f1c67
Usuario <pablot.pavan@gmail.com>
2023-09-18 10:13:43 -0300

    pequeño cambio

--- pokedex.sql
+++ pokedex.sql
@@ -1,14 +1,14 @@
 CREATE SCHEMA IF NOT EXISTS pokedex;
 USE pokedex;
 CREATE TABLE pokemon(
-                        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL UNIQUE,
+                        id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
                         num_id INT,
                         imagen VARCHAR(50),
                         nombre VARCHAR(50),
                         tipo VARCHAR(50),
                         descripcion VARCHAR(50));
 CREATE TABLE usuario(
-    usuario VARCHAR(50) PRIMARY KEY NOT NULL UNIQUE,
+    usuario VARCHAR(50) PRIMARY KEY NOT NULL,
     clave VARCHAR(50) NOT NULL
 );
 INSERT INTO `pokemon` (`id`, `num_id`, `imagen`, `nombre`, `tipo`, `descripcion`) VALUES
